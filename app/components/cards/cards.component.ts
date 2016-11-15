8ed430453ab6c632d3b4329999893025397562dc
Nazar <nazarbaker24@gmail.com>
2016-11-15 23:10:55 +0200

    add jquery

--- app/components/cards/cards.component.ts
+++ app/components/cards/cards.component.ts
@@ -2,25 +2,36 @@ import { Component } from '@angular/core';
 import { ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { WordsService } from '../../services/words.service';
+import { Injectable } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
     selector: 'my-cards',
     templateUrl: 'components/cards/cards.component.html',
     styleUrls: ['components/cards/cards.component.css']
 })
-export class CardsComponent {
+@Injectable()
+export class CardsComponent implements OnInit {
   english: string;
   ukrainian: string;
   _id: string;
   number: number;
 
-    constructor(private _elRef: ElementRef, private wordsService: WordsService) {
+  constructor(private _elRef: ElementRef, private wordsService: WordsService) {
     this.wordsService.getWords()
-        .subscribe(words => {
-          this.number = Math.floor(Math.random() * words.length)
-          this.words = words
-          this.english = this.words[this.number].english
-          this.ukrainian = this.words[this.number].ukrainian
-        });
+      .subscribe(words => {
+        this.number = Math.floor(Math.random() * words.length)
+        this.words = words
+        this.english = this.words[this.number].english
+        this.ukrainian = this.words[this.number].ukrainian
+      });
+  }
+
+  ngOnInit():any {
+    jQuery(this._elRef.nativeElement).find('.flashcard').on('click', function() {
+      jQuery('.flashcard').toggleClass('flipped');
+    });
   }
 }
